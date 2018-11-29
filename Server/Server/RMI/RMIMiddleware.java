cc26ebc8d9b3080563f0db18404869df6554f07c
alicezhu <24253275+spicyAway@users.noreply.github.com>
2018-11-29 14:49:13 -0500

    Debug RMIMiddleware

--- Server/Server/RMI/RMIMiddleware.java
+++ Server/Server/RMI/RMIMiddleware.java
@@ -598,7 +598,9 @@ public class RMIMiddleware extends ResourceManager
       providedAddresses[2] = args[5];
 
     }else{
-      System.out.print("DEBUG LENGTH: " + args.length + "\n");
+      for(int i=0 ; i<args.length; i++){
+        System.out.print("DEBUG: " + args[i] + "\n");
+      }
       System.out.println("Required usage: [firstResourceManager] [address1] [secondResourceManager] [address2] [thirdResourceManager] [address3] ");
       System.exit(1);
     }
